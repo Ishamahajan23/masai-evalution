ee7a89bd41bc075336da5c60fcf9fece8e2fce4e
Ishamahajan23 <ishamahajan933@gmail.com>
2026-01-16 17:26:01 +0530

    add

--- Placement-days/day13/node/server.js
+++ Placement-days/day13/node/server.js
@@ -1,40 +1,25 @@
 import express from 'express';
+import verificationMiddleware from './middleware/verification.middleware.js';
+import errorHandler from './middleware/errorhandler.js';
 import cors from 'cors';
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// global logger (VERY IMPORTANT)
-app.use((req, res, next) => {
-  console.log('HIT ->', req.method, req.url);
-  next();
-});
-
-const verificationMiddleware = (req, res, next) => {
-  console.log('MIDDLEWARE BODY:', req.body);
-
-  const { email, password, age } = req.body;
-  const errors = [];
-
-  if (!email) errors.push('email missing');
-  if (!password) errors.push('password missing');
-  if (!age) errors.push('age missing');
-
-  if (errors.length) {
-    return res.status(400).json({ errors });
-  }
-
-  next();
-};
 
 app.post('/register', verificationMiddleware, (req, res) => {
-  res.json({ message: 'SUCCESS' });
+  res.status(200).json({ message: 'User registered successfully' });
 });
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log('Server running on 5000');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
